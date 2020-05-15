f71ca3c861577f94228c3d22e3fb92c77d9d845e
Phuc Nguyen <anicedream2002@gmail.com>
2020-05-15 14:46:06 -0500

    mac_branch

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize('data_cython.py'))
+setup(ext_modules = cythonize('data_cython.py'), requires=['xlrd'])
